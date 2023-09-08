b8ee79f5ff0d4f336ca424587cf4610b3dcf0756
Mekelaina <chloerae018@gmail.com>
2023-09-08 02:36:23 -0400

    passign data segfaults idk man

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
-//#include <stdio.h>
+#include <stdio.h>
 //#include "tokens.h"
 #include "tokenizer.h"
 
-char* input = "exit 0;";
+char* input = "exit(0);";
 
 
 int main(){
 
 
-    tokenize(input);
+    Token *tokens = tokenize(input);
+
+    for(int i = 0; i < LIST_LENGTH; i++){
+        printf("TokenType: %d, TokenValueP: %s", tokens[i].type, tokens[i].value);
+    }
 
     return 0;
 }
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,20 +1,50 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "tokenizer.h"
 
-Token TokenBuffer[16];
+
 
 Token* tokenize(char* text){
+    Token tokenBuffer[16];
+    int tokcount = 0;
     int i = 0;
     char c;
+    char* buff;
+
     for(;;){
         c = text[i];
         if(c == NULL){
             break;
         }
-        printf("%c", c);        
+
+        switch (c){
+            case '(':
+                Token f, p;
+                f.type = tt_funCall;
+                f.value = buff;
+                tokenBuffer[tokcount++] = f;
+                buff = "";
+                p.type = tt_lParen;
+                p.value = "(";
+                tokenBuffer[tokcount++] = p;
+            case ')':
+                Token t;
+                t.type = tt_rParen;
+                t.value = ")";
+                tokenBuffer[tokcount++] = t;
+            case ';':
+                Token t2;
+                t2.type = tt_semicolon;
+                t2.value = ";";
+                tokenBuffer[tokcount++] = t;
+            default:
+                strcat(buff, c);
+        }
+        
+
         i++;
     }
-    return NULL;
+    return tokenBuffer;
 }
